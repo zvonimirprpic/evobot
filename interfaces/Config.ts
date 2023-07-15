8819ed298d4c6438ac83b9b05fa7bbf847a415e3
unknown <zvonimirprpic@yahoo.com>
2023-07-16 00:16:31 +0200

    fixing deploy

--- interfaces/Config.ts
+++
@@ -1,9 +0,0 @@
-export interface Config {
-  TOKEN: string;
-  PREFIX: string;
-  MAX_PLAYLIST_SIZE: number;
-  PRUNING: boolean;
-  STAY_TIME: number;
-  DEFAULT_VOLUME: number;
-  LOCALE: string;
-}
